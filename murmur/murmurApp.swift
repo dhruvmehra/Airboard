9b0d83c38e56f646cc2205f753740ee1b93bd85c
dhruv <dhruv@pypeai.com>
2025-12-01 01:02:07 +0530

    Initial Commit

---
+++ murmur/murmurApp.swift
@@ -0,0 +1,17 @@
+//
+//  murmurApp.swift
+//  murmur
+//
+//  Created by Dhruv Mehra on 01/12/25.
+//
+
+import SwiftUI
+
+@main
+struct murmurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
